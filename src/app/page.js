1579d936c5820456d9ec61c37b84e1a094ab83ee
HyunsuShin00 <hyunsu@space-vision.ai>
2024-08-28 06:16:33 +0900

    0828 b

--- src/app/page.js
+++ src/app/page.js
@@ -16,17 +16,18 @@ export default function Home() {
         style={{
           background: "linear-gradient(180deg, #FFE072 0%, #EBFC72 100%)",
         }}
-        className="w-full  flex flex-col items-center justify-center"
+        className="relative w-full  flex flex-col items-center justify-center"
       >
         <div className="flex items-center flex-col max-w-7xl w-full">
           <Image src="/assets/Puggy_Logo.svg" alt="Logo" width={300} height={100} className="mt-10 w-28 md:w-80 " />
           <Ttile str="PUGGY STAKING" />
-          <div className="flex px-4  w-full py-2 md:space-x-8  justify-center ">
+          <div className="flex px-4 pb-8 md:pb-20  w-full pt-2  md:space-x-8  justify-center ">
             <IntroLeft />
             <IntroCenter />
             <IntroRight />
           </div>
         </div>
+        <Image src="/assets/Puggy_1.png" alt="Puggy_2" width={300} height={100} className="absolute -bottom-20 md:-bottom-40 right-0 z-10 w-40 md:w-60" />
       </div>
       <div className="relative w-full px-4 flex flex-col items-center  bg-[#814C12]  py-8">
         <MYPuggy />
