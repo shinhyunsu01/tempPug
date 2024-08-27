6d40c55ab6c592022664b20b156fb73e49d25c2a
HyunsuShin00 <hyunsu@space-vision.ai>
2024-08-28 06:45:01 +0900

    0828 e

--- src/app/components/MYPuggy.js
+++ src/app/components/MYPuggy.js
@@ -6,10 +6,10 @@ export default function MYPuggy() {
   const titleArr = ["Total Staked", "Available $PUGGY", "Staking Amount", "Reward Received", "Reward Claimable"];
 
   return (
-    <div className="max-w-7xl w-full h-full rounded-2xl flex items-center flex-col  bg-[#F9A03ECC] bg-opacity-80 shadow-xl pb-10 px-10 my-8">
+    <div className=" max-w-7xl w-full h-full rounded-2xl flex items-center flex-col  bg-[#F9A03ECC] bg-opacity-80 shadow-xl pb-10 px-10 my-8">
       <Ttile str="MY PUGGY" />
       <div className="w-full">
-        <div className="font-pretendard w-full grid grid-cols-5 pb-6">
+        <div className="font-pretendard w-full grid grid-cols-5 pb-6 text-xs md:text-sm">
           {titleArr.map((title) => (
             <div className="text-center" key={title}>
               {title}
@@ -17,7 +17,7 @@ export default function MYPuggy() {
           ))}
         </div>
         <hr className="w-full border-black border-3px " />
-        <div className="font-pretendard w-full grid grid-cols-5  py-4">
+        <div className="font-pretendard w-full grid grid-cols-5  py-4 text-xs">
           <div className="text-center">0</div>
           <div className="text-center">0</div>
           <div className="text-center">0</div>
