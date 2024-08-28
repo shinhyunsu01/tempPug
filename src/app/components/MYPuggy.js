231fb02b62134a07d98d9474a1d0b76ae5fe3062
HyunsuShin00 <hyunsu@space-vision.ai>
2024-08-28 23:57:29 +0900

    0827 stake ㅊ

--- src/app/components/MYPuggy.js
+++ src/app/components/MYPuggy.js
@@ -9,7 +9,7 @@ export default function MYPuggy() {
   const titleArr = ["Total Staked", "Available $PUGGY", "Staking Amount", "Reward Received", "Reward Claimable"];
   const userTotalStaked = useRecoilValue(userTotalStakedRecoil);
   const userAvailablePuggy = useRecoilValue(userAvailablePuggyRecoil);
-  const userStakingAmount = useRecoilValue(userStakingAmountRecoil);
+  //const userStakingAmount = useRecoilValue(userStakingAmountRecoil);
   const totalStaked = useRecoilValue(totalStakedRecoil);
 
   const [userData, setUserData] = useState({
@@ -21,8 +21,8 @@ export default function MYPuggy() {
   useEffect(() => {
     if (totalStaked != null) setUserData((prevState) => ({ ...prevState, totalStaked: String(totalStaked) }));
     if (userAvailablePuggy != null) setUserData((prevState) => ({ ...prevState, userAvailablePuggy: String(userAvailablePuggy) }));
-    if (userStakingAmount != null) setUserData((prevState) => ({ ...prevState, userStakingAmount: String(userStakingAmount) }));
-  }, [userAvailablePuggy, userStakingAmount, totalStaked]);
+    if (userTotalStaked != null) setUserData((prevState) => ({ ...prevState, userTotalStaked: String(userTotalStaked) }));
+  }, [userAvailablePuggy, userTotalStaked, totalStaked]);
 
   return (
     <div className=" max-w-7xl w-full h-full rounded-2xl flex items-center flex-col  bg-[#F9A03ECC] bg-opacity-80 shadow-xl pb-10 px-10 my-8">
@@ -39,7 +39,7 @@ export default function MYPuggy() {
         <div className="font-pretendard w-full grid grid-cols-5  py-4 text-xs">
           <div className="text-center">{userData.totalStaked === null ? 0 : userData.totalStaked}</div>
           <div className="text-center">{userData.userAvailablePuggy === null ? 0 : userData.userAvailablePuggy}</div>
-          <div className="text-center">{userData.userStakingAmount === null ? 0 : userData.userStakingAmount}</div>
+          <div className="text-center">{userData.userTotalStaked === null ? 0 : userData.userTotalStaked}</div>
           <div className="text-center">0</div>
           <div className="text-center">0</div>
         </div>
